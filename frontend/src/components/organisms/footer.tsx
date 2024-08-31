79b9df8839660e7c6d18bf072355df586e242f97
Francis KOUAHO <kouahofrancis@gmail.com>
2024-08-31 15:21:31 +0200

    feat(frontend): add a new footer

--- frontend/src/components/organisms/footer.tsx
+++ frontend/src/components/organisms/footer.tsx
@@ -1,15 +1,19 @@
-export default function Footer() {
-  return (
-    <footer className="m500:text-sm dark:bg-darkBg z-30 bg-white px-5 py-5 text-center font-base">
-      Released under MIT License. The source code is available on{' '}
-      <a
-        target="_blank"
-        href="https://github.com/neobrutalism-templates/saas"
-        className="font-heading underline"
-      >
-        Github
-      </a>
-      .
-    </footer>
-  )
+'use client'
+
+import {useEffect, useState} from 'react';
+
+const Footer = () => {
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        setYear(new Date().getFullYear());
+    }, []);
+
+    return (
+        <footer className="m500:text-sm dark:bg-darkBg text-sm z-30 bg-white px-5 py-5 text-center font-base">
+            © All rights reserved || {year} || Made with ❤️ jobpilot
+        </footer>
+    );
 }
+
+export default Footer;
